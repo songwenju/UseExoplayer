d9e9db0e2ab6189554611da033f29a020a41b921
songwj2 <songwj2@lenovo.com>
2017-05-09 19:37:51 +0800

    add the moudle

---
+++ exoplayerdemo/src/main/java/com/songwenju/exoplayerdemo/MainActivity.java
@@ -0,0 +1,362 @@
+package com.songwenju.exoplayerdemo;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DataSpec;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.FileDataSource;
+import com.google.android.exoplayer2.upstream.RawResourceDataSource;
+import com.google.android.exoplayer2.util.Util;
+import com.songwenju.androidtools.util.LogUtil;
+
+import java.io.File;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SimpleExoPlayer exoPlayer;
+    private ExoPlayer.EventListener eventListener = new ExoPlayer.EventListener() {
+        @Override
+        public void onTimelineChanged(Timeline timeline, Object manifest) {
+            LogUtil.i(TAG,"onTimelineChanged");
+        }
+
+        @Override
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+            LogUtil.i(TAG,"onTracksChanged");
+        }
+
+        @Override
+        public void onLoadingChanged(boolean isLoading) {
+            LogUtil.i(TAG,"onLoadingChanged");
+        }
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            LogUtil.i(TAG,"onPlayerStateChanged: playWhenReady = "+String.valueOf(playWhenReady)
+                    +" playbackState = "+playbackState);
+            switch (playbackState){
+                case ExoPlayer.STATE_ENDED:
+                    LogUtil.i(TAG,"Playback ended!");
+                    //Stop playback and return to start position
+                    setPlayPause(false);
+                    exoPlayer.seekTo(0);
+                    break;
+                case ExoPlayer.STATE_READY:
+                    LogUtil.i(TAG,"ExoPlayer ready! pos: "+exoPlayer.getCurrentPosition()
+                            +" max: "+stringForTime((int)exoPlayer.getDuration()));
+                    setProgress();
+                    break;
+                case ExoPlayer.STATE_BUFFERING:
+                    LogUtil.i(TAG,"Playback buffering!");
+                    break;
+                case ExoPlayer.STATE_IDLE:
+                    LogUtil.i(TAG,"ExoPlayer idle!");
+                    break;
+            }
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException error) {
+            LogUtil.i(TAG,"onPlaybackError: "+error.getMessage());
+        }
+
+        @Override
+        public void onPositionDiscontinuity() {
+            LogUtil.i(TAG,"onPositionDiscontinuity");
+        }
+    };
+
+    private SeekBar seekPlayerProgress;
+    private Handler handler;
+    private ImageButton btnPlay;
+    private TextView txtCurrentTime, txtEndTime;
+    private boolean isPlaying = false;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        File f_ext_files_dir = getExternalFilesDir(null);
+
+        FileDialog fileDiaLogUtil = new FileDialog(this, f_ext_files_dir, "");
+        fileDiaLogUtil.addFileListener(new FileDialog.FileSelectedListener() {
+            @Override
+            public void fileSelected(File file) {
+                LogUtil.i("File selected:",file.getAbsolutePath());
+                prepareExoPlayerFromFileUri(Uri.fromFile(file));
+
+                /*
+                try {
+                    FileInputStream inputStream = new FileInputStream(file);
+                    byte[] fileData = new byte[(int)file.length()];
+                    LogUtil.i(TAG,"Data before read: "+fileData.length);
+                    int bytesRead = inputStream.read(fileData);
+                    LogUtil.i(TAG,"Bytes read: "+bytesRead);
+                    if(bytesRead>0) {
+                        prepareExoPlayerFromByteArray(fileData);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                */
+            }
+        });
+        //fileDiaLogUtil.showDiaLogUtil();
+
+        //prepareExoPlayerFromRawResourceUri(RawResourceDataSource.buildRawResourceUri(R.raw.audio));
+
+        prepareExoPlayerFromURL(Uri.parse("https://github.com/nzkozar/ExoplayerExample/blob/master/sample.m4a?raw=true"));
+    }
+
+    //TODO
+    private void prepareExoPlayerFromByteArray(byte[] data){
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(null), new DefaultLoadControl());
+        exoPlayer.addListener(eventListener);
+
+        final MByteArrayDataSource byteArrayDataSource = new MByteArrayDataSource(data);
+        LogUtil.i(TAG,"ByteArrayDataSource constructed.");
+        /*
+        DataSpec dataSpec = new DataSpec(byteArrayDataSource.getUri());
+        try {
+            byteArrayDataSource.open(dataSpec);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+        DataSource.Factory factory = new DataSource.Factory() {
+            @Override
+            public DataSource createDataSource() {
+                return byteArrayDataSource;
+            }
+        };
+        LogUtil.i(TAG,"DataSource.Factory constructed.");
+
+        MediaSource audioSource = new ExtractorMediaSource(byteArrayDataSource.getUri(),
+                factory, new DefaultExtractorsFactory(),null,null);
+        LogUtil.i(TAG,"Audio source constructed.");
+        exoPlayer.prepare(audioSource);
+        initMediaControls();
+    }
+
+    /**
+     * Prepares exoplayer for audio playback from a local file
+     * @param uri
+     */
+    private void prepareExoPlayerFromFileUri(Uri uri){
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(null), new DefaultLoadControl());
+        exoPlayer.addListener(eventListener);
+
+        DataSpec dataSpec = new DataSpec(uri);
+        final FileDataSource fileDataSource = new FileDataSource();
+        try {
+            fileDataSource.open(dataSpec);
+        } catch (FileDataSource.FileDataSourceException e) {
+            e.printStackTrace();
+        }
+
+        DataSource.Factory factory = new DataSource.Factory() {
+            @Override
+            public DataSource createDataSource() {
+                return fileDataSource;
+            }
+        };
+        MediaSource audioSource = new ExtractorMediaSource(fileDataSource.getUri(),
+                factory, new DefaultExtractorsFactory(), null, null);
+
+        exoPlayer.prepare(audioSource);
+        initMediaControls();
+    }
+
+
+    /**
+     * Prepares exoplayer for audio playback from a remote URL audiofile. Should work with most
+     * popular audiofile types (.mp3, .m4a,...)
+     * @param uri Provide a Uri in a form of Uri.parse("http://blabla.bleble.com/blublu.mp3)
+     */
+    private void prepareExoPlayerFromURL(Uri uri){
+
+        TrackSelector trackSelector = new DefaultTrackSelector();
+
+        LoadControl loadControl = new DefaultLoadControl();
+
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), null);
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource audioSource = new ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null);
+        exoPlayer.addListener(eventListener);
+
+        exoPlayer.prepare(audioSource);
+        initMediaControls();
+    }
+
+    private void prepareExoPlayerFromRawResourceUri(Uri uri){
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(null), new DefaultLoadControl());
+        exoPlayer.addListener(eventListener);
+
+        DataSpec dataSpec = new DataSpec(uri);
+        final RawResourceDataSource rawResourceDataSource = new RawResourceDataSource(this);
+        try {
+            rawResourceDataSource.open(dataSpec);
+        } catch (RawResourceDataSource.RawResourceDataSourceException e) {
+            e.printStackTrace();
+        }
+
+        DataSource.Factory factory = new DataSource.Factory() {
+            @Override
+            public DataSource createDataSource() {
+                return rawResourceDataSource;
+            }
+        };
+
+        MediaSource audioSource = new ExtractorMediaSource(rawResourceDataSource.getUri(),
+                factory, new DefaultExtractorsFactory(), null, null);
+
+        exoPlayer.prepare(audioSource);
+        initMediaControls();
+    }
+
+    private void initMediaControls() {
+        initPlayButton();
+        initSeekBar();
+        initTxtTime();
+    }
+
+    private void initPlayButton() {
+        btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+        btnPlay.requestFocus();
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setPlayPause(!isPlaying);
+            }
+        });
+    }
+
+    /**
+     * Starts or stops playback. Also takes care of the Play/Pause button toggling
+     * @param play True if playback should be started
+     */
+    private void setPlayPause(boolean play){
+        isPlaying = play;
+        exoPlayer.setPlayWhenReady(play);
+        if(!isPlaying){
+            btnPlay.setImageResource(android.R.drawable.ic_media_play);
+        }else{
+            setProgress();
+            btnPlay.setImageResource(android.R.drawable.ic_media_pause);
+        }
+    }
+
+    private void initTxtTime() {
+        txtCurrentTime = (TextView) findViewById(R.id.time_current);
+        txtEndTime = (TextView) findViewById(R.id.player_end_time);
+    }
+
+    private String stringForTime(int timeMs) {
+        StringBuilder mFormatBuilder;
+        Formatter mFormatter;
+        mFormatBuilder = new StringBuilder();
+        mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
+        int totalSeconds =  timeMs / 1000;
+
+        int seconds = totalSeconds % 60;
+        int minutes = (totalSeconds / 60) % 60;
+        int hours   = totalSeconds / 3600;
+
+        mFormatBuilder.setLength(0);
+        if (hours > 0) {
+            return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString();
+        } else {
+            return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+        }
+    }
+
+    private void setProgress() {
+        seekPlayerProgress.setProgress(0);
+        seekPlayerProgress.setMax((int) exoPlayer.getDuration()/1000);
+        txtCurrentTime.setText(stringForTime((int)exoPlayer.getCurrentPosition()));
+        txtEndTime.setText(stringForTime((int)exoPlayer.getDuration()));
+
+        if(handler == null)handler = new Handler();
+        //Make sure you update Seekbar on UI thread
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (exoPlayer != null && isPlaying) {
+                    seekPlayerProgress.setMax((int) exoPlayer.getDuration()/1000);
+                    int mCurrentPosition = (int) exoPlayer.getCurrentPosition() / 1000;
+                    seekPlayerProgress.setProgress(mCurrentPosition);
+                    txtCurrentTime.setText(stringForTime((int)exoPlayer.getCurrentPosition()));
+                    txtEndTime.setText(stringForTime((int)exoPlayer.getDuration()));
+
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        });
+    }
+
+    private void initSeekBar() {
+        seekPlayerProgress = (SeekBar) findViewById(R.id.mediacontroller_progress);
+        seekPlayerProgress.requestFocus();
+
+        seekPlayerProgress.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (!fromUser) {
+                    // We're not interested in programmatically generated changes to
+                    // the progress bar's position.
+                    return;
+                }
+
+                exoPlayer.seekTo(progress*1000);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekPlayerProgress.setMax(0);
+        seekPlayerProgress.setMax((int) exoPlayer.getDuration()/1000);
+
+    }
+}
